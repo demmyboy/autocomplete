d4b725f4e63a51d489c3869ec5b3fc53577b0518
demmyboy <demolaabdulai@live.com>
2016-04-01 12:36:15 +0200

    modiifed the js again

--- javascript/mapsapplication.js
+++ javascript/mapsapplication.js
@@ -1,30 +1,32 @@
 var mapApplication = function (){
 
 	//add member here
-
+	console.log(mapApplication);
 	var mapsModel = {
 		fromAddress: ko.observable(),
 		toAddress: ko.observable()
 	};
-	// method to add custom binding handler to the KO
-	var configureBindingHandlers = function(){
+	
+	
+	var init = function(){
+		// method to add custom binding handler to the KO
+		var configureBindingHandlers = function(){
 		// custom binding for address auto complete
-		ko.bindingHandlers.addressAutoComplete = {
-			init: function(element, valueAccessor){
-				//create autocomplete object
-				var autocomplete = new google.maps.places.Autocomplete(element, {types:['geocode']});
-				// when user selects an address from the drop down, populate the address in the model.
-				var value = valueAccessor();
-				google.maps.event.addListener(autocomplete, 'place_changed', function(){
-					var place = autocomplete.getPlace();
-					console.log(place);
-					value(place);
+			ko.bindingHandlers.addressAutoComplete = {
+				init: function(element, valueAccessor){
+					//create autocomplete object
+					var autocomplete = new google.maps.places.Autocomplete(element, {types:['geocode']});
+					// when user selects an address from the drop down, populate the address in the model.
+					var value = valueAccessor();
+					google.maps.event.addListener(autocomplete, 'place_changed', function(){
+						var place = autocomplete.getPlace();
+						console.log(place);
+						value(place);
 				});
 			}
 		};
 	};
-	
-	var init = function(){
+
 	// add code to initialise the module
 		ko.applyBindings(mapApplication);
 	};
@@ -37,3 +39,4 @@ var mapApplication = function (){
 	mapsModel: mapsModel
 	};
 }();
+   
